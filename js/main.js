11e47735f2619156bf48f71389f7784aab232843
Keisuke_Shimizu <simizu26629@outlook.jp>
2020-08-31 11:30:37 +0900

    課題1-3完了

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
-'use strict'
+'use strict' 
 {
-  let boolean = false;
-  const capital = '東京';
-  
-  do {
-    const result = prompt('日本の首都は？');
-    if (capital === result) {
-      alert('正解です！');
-      boolean = true;
-    } else {
-      alert('不正解です！');
-    }
-  } while (boolean === false);
+  const header = ['========================', '現在持っているのタスク一覧', '========================'];
+  const task = ['掃除', '買い物', '散歩'];
 
+  function showTask() {
+    for (let i = 0; i < header.length; i++) {
+      console.log(header[i]);
+    }
+    for (let i = 0; i < task.length; i++) {
+      console.log(`${i} : ${task[i]}`);
+    }
+  }
+
+  showTask();
+
+  let result = prompt('タスクを入力してください')
+  task.push(result);
+  
+  alert('新しいタスクを追加しました');
+
+  showTask();
+
+  prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
 
 }
