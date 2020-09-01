af398a3ca062e75ec0774d35a461bcd56458e866
Keisuke_Shimizu <simizu26629@outlook.jp>
2020-09-01 15:35:59 +0900

    課題1-4完了

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 'use strict' 
 {
   const header = ['========================', '現在持っているのタスク一覧', '========================'];
-  const tasks = ['掃除', '買い物', '散歩'];
+  const tasks = [
+    {content: '机を片付ける', genre: '掃除'},
+    {content: '牛乳を買う', genre: '買い物'},
+    {content: '散歩する', genre: '運動'},
+  ];
 
   function showTask() {
     header.forEach(element => console.log(element));
-    tasks.forEach(task => console.log(task));
+    tasks.forEach((task, index) => {
+      console.log(`${index} ： [内容]${task.content}、[ジャンル]${task.genre}`)
+    });
   }
 
   showTask();
 
-  tasks.push(prompt('タスクを入力してください'));
+  const newContent = prompt('タスクを入力してください')
+  const newGenre = prompt('ジャンルを入力してください')
+
+  tasks.push({content: newContent, genre: newGenre});
   
   alert('新しいタスクを追加しました');
 
